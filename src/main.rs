cac25e5b7f37251e4c3d976058f583610db7cef4
Rong Ou <rong.ou@gmail.com>
2025-10-16 16:16:29 -0700

    Revert "Keep prompt anchored with external printer"
    This reverts commit 7e44ce488adc98bffbf0b3c8d64eb9ecab8f3f0a.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, ExternalPrinter};
 
 fn main() -> Result<(), ReadlineError> {
     run_repl()
@@ -7,31 +7,17 @@ fn main() -> Result<(), ReadlineError> {
 
 fn run_repl() -> Result<(), ReadlineError> {
     let mut editor = DefaultEditor::new()?;
-    let mut printer = editor.create_external_printer().ok();
-
     loop {
         match editor.readline("aiome> ") {
             Ok(line) => {
-                if let Some(printer) = printer.as_mut() {
-                    printer.print(format!("{line}\n"))?;
-                } else {
-                    println!("{line}");
-                }
+                println!("{line}");
                 editor.add_history_entry(line)?;
             }
             Err(ReadlineError::Interrupted) => {
-                if let Some(printer) = printer.as_mut() {
-                    printer.print("^C\n".to_string())?;
-                } else {
-                    println!("^C");
-                }
+                println!("^C");
             }
             Err(ReadlineError::Eof) => {
-                if let Some(printer) = printer.as_mut() {
-                    printer.print("\n".to_string())?;
-                } else {
-                    println!();
-                }
+                println!();
                 break;
             }
             Err(err) => {
