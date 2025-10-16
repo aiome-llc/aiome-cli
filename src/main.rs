7e44ce488adc98bffbf0b3c8d64eb9ecab8f3f0a
Rong Ou <rong.ou@gmail.com>
2025-10-16 16:10:57 -0700

    Keep prompt anchored with external printer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use rustyline::DefaultEditor;
 use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, ExternalPrinter};
 
 fn main() -> Result<(), ReadlineError> {
     run_repl()
@@ -7,17 +7,31 @@ fn main() -> Result<(), ReadlineError> {
 
 fn run_repl() -> Result<(), ReadlineError> {
     let mut editor = DefaultEditor::new()?;
+    let mut printer = editor.create_external_printer().ok();
+
     loop {
         match editor.readline("aiome> ") {
             Ok(line) => {
-                println!("{line}");
+                if let Some(printer) = printer.as_mut() {
+                    printer.print(format!("{line}\n"))?;
+                } else {
+                    println!("{line}");
+                }
                 editor.add_history_entry(line)?;
             }
             Err(ReadlineError::Interrupted) => {
-                println!("^C");
+                if let Some(printer) = printer.as_mut() {
+                    printer.print("^C\n".to_string())?;
+                } else {
+                    println!("^C");
+                }
             }
             Err(ReadlineError::Eof) => {
-                println!();
+                if let Some(printer) = printer.as_mut() {
+                    printer.print("\n".to_string())?;
+                } else {
+                    println!();
+                }
                 break;
             }
             Err(err) => {
